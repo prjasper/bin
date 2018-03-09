e414e75cdbcc7eda7b9cf7b200cacf80e488d8ce
Paul Jasper <prjasper@users.noreply.github.com>
2018-03-08 17:44:44 -0800

    Add "--tags:Purpose=PURPOSE" when running pipelines (#4)

--- ro_submit.sh
+++ ro_submit.sh
@@ -3,7 +3,7 @@
 usage() {
     NAME=`basename $0`
     cat <<EOF
-Usage: ${NAME} [-r|-c|-s|-m|-h] [-d|-t] [-g] DATE [COMMENT]
+Usage: ${NAME} [-r|-c|-s|-m|-h] [-d|-t] [-p PURPOSE] [-g] DATE [COMMENT]
 Run the Reach & Overlap pipeline for the given DATE, adding an optional COMMENT to the name.
 The current directory should be the base of the krux-spark-marketer repo.
 The "-r" option calculates regular site reach and overlaps. This is the default.
@@ -12,7 +12,8 @@ The "-s" option calculates segment reach and overlaps using S3 user-segment data
 The "-m" option calculates segment reach and overlaps using the unreliable marketer impressions.
 The "-n" option calculates all segment reach and overlaps for an organization simultaneously, which can cause out-of-memory conditions.
 The "-d" option runs for daily clients only. The default is to run weekly clients only.
-The "-t" option runs for retry clients only.
+The "-t" option runs for retry clients only and sets the Purpose tag to "rerun".
+The "-p" option sets the Purpose tag to the given PURPOSE, which must be one of "scheduled", "manual", "backfill", "rerun" or "test".
 The "-g" option generates the configuration without running it.
 The "-h" option prints this usage message.
 EOF
@@ -21,7 +22,8 @@ EOF
 TYPE=Site
 GENERATE=0
 DAILY=
-while getopts "crsmndtgh" OPT; do
+PURPOSE=manual
+while getopts "crsmndtp:gh" OPT; do
     case "$OPT" in
         r)
             TYPE=Site
@@ -43,6 +45,23 @@ while getopts "crsmndtgh" OPT; do
             ;;
         t)
             DAILY=Retry
+            PURPOSE=rerun
+            ;;
+        p)
+            case ${OPTARG} in
+                scheduled|manual|backfill|rerun|test)
+                    PURPOSE=${OPTARG}
+                    ;;
+                *)
+                    echo "Invalid PURPOSE: ${OPTARG}" >&2
+                    usage >&2
+                    ;;
+            esac
+            PURPOSE=${OPTARG}
+            ;;
+        :)
+            echo "Option -${OPTARG} requires an argument" >&2
+            exit 1
             ;;
         g)
             GENERATE=1
@@ -76,6 +95,6 @@ fi
 export KRUX_ENVIRONMENT=prod
 sbt <<EOF
 project marketerAnalytics
-run-main com.krux.marketer.reachoverlap.pipeline.ReachOverlap${DAILY}${TYPE}Pipeline ${COMMAND}
+run-main com.krux.marketer.reachoverlap.pipeline.ReachOverlap${DAILY}${TYPE}Pipeline ${COMMAND} --tags:Purpose=${PURPOSE}
 exit
 EOF
